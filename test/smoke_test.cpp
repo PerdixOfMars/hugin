688640622574dcaf8c139479ff0a2ab47142c521
Perdix of Mars <perdix.p76l@gmail.com>
2026-05-18 18:44:55 +0000

    Link Hugin tests against live Munin

--- test/smoke_test.cpp
+++ test/smoke_test.cpp
@@ -1,6 +1,16 @@
 #include <cstdlib>
 
+#include <munin/container.hpp>
+
 int main()
 {
+    munin::container container;
+    auto const snapshot = container.to_json();
+
+    if (snapshot["type"] != "container")
+    {
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
