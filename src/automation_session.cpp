c1b2204d621cc20226e478b0030415e245592287
PerdixOfMars <perdix.p76l@googlemail.com>
2026-05-25 11:46:52 +0000

    Reuse snapshot capture in automation session

--- src/automation_session.cpp
+++ src/automation_session.cpp
@@ -1,5 +1,7 @@
 #include "hugin/automation_session.hpp"
 
+#include "hugin/munin_snapshot.hpp"
+
 namespace hugin {
 
 automation_session::automation_session(munin::component &root) : root_(root)
@@ -8,7 +10,7 @@ automation_session::automation_session(munin::component &root) : root_(root)
 
 auto automation_session::snapshot() const -> nlohmann::json
 {
-    return root_.to_json();
+    return capture_snapshot(root_);
 }
 
 void automation_session::press_key(terminalpp::virtual_key const &key)
