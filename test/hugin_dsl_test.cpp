0e224181126f7c49da7e52f2303c7b552907eb5a
PerdixOfMars <perdix.p76l@googlemail.com>
2026-06-01 09:16:02 +0000

    Share Hugin keypress test helper

--- test/hugin_dsl_test.cpp
+++ test/hugin_dsl_test.cpp
@@ -106,22 +106,20 @@ protected:
             .get<std::string>();
     }
 
-    static auto tab_key() -> terminalpp::virtual_key
+    static auto keypress(terminalpp::vk key) -> terminalpp::virtual_key
     {
         return terminalpp::virtual_key{
-            terminalpp::vk::ht,
-            terminalpp::vk_modifier::none,
-            1,
-            terminalpp::byte{'\t'}};
+            key, terminalpp::vk_modifier::none, 1, terminalpp::byte{'\t'}};
+    }
+
+    static auto tab_key() -> terminalpp::virtual_key
+    {
+        return keypress(terminalpp::vk::ht);
     }
 
     static auto back_tab_key() -> terminalpp::virtual_key
     {
-        return terminalpp::virtual_key{
-            terminalpp::vk::bt,
-            terminalpp::vk_modifier::none,
-            1,
-            terminalpp::byte{'\t'}};
+        return keypress(terminalpp::vk::bt);
     }
 
     static void add_button_under_intermediate_containers(
